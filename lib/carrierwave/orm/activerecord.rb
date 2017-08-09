d7602eaf2cef7e37790b2960f0b84c56bd24cf08
Artem Adamtsov <aay@mesto.ru>
2017-08-09 14:20:31 +0300

    Fix serializable_hash

--- lib/carrierwave/orm/activerecord.rb
+++ lib/carrierwave/orm/activerecord.rb
@@ -60,7 +60,7 @@ module CarrierWave
 
           self.class.uploaders.each do |column, uploader|
             if (!only && !except) || (only && only.include?(column.to_s)) || (!only && except && !except.include?(column.to_s))
-              hash[column.to_s] = _mounter(column).uploader.serializable_hash
+              hash[column.to_s] = _mounter(column).uploaders.first.serializable_hash
             end
           end
           super(options).merge(hash)
